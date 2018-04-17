65eb76cd1157b9ce3c1edf5091ac6bf99833aab2
wangqiang1993 <271632139@qq.com>
2018-04-17 22:56:33 +0800

    提交springboot项目

---
+++ src/main/java/com/scu/wang/Bean.java
@@ -0,0 +1,25 @@
+package com.scu.wang;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Bean {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private Integer age;
+
+    public Bean() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
